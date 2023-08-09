3fa51a18be326d715c2711f298eef50315ef835d
Servando Hernandez <s_musicpia@yahoo.com.mx>
2023-08-08 19:14:33 -0600

    Create DataContext and Entity Country in Sales.API

---
+++ Sales.Shared/Entities/Country.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.Shared.Entities
+{
+	public class Country
+	{
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El campo {0} es obligatorio.")]
+        [MaxLength(100, ErrorMessage ="El campo {0} no puede tener mas de {1} caracteres.")]
+        [Display(Name ="Pais")]
+        public string Name { get; set; } = null!;
+    }
+}
