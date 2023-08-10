a55baad11790b9165067e371406c42973b794b2f
Servando Hernandez Hernandez <s_musicpia@yahoo.com.mx>
2023-08-10 15:26:07 -0600

    ADD DataContext Create DB Sales and Table Countries

--- Sales.Shared/Entities/Country.cs
+++ Sales.Shared/Entities/Country.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Sales.Shared.Entities
 {
-	public class Country
-	{
+    public class Country
+    {
         public int Id { get; set; }
 
-        [Required(ErrorMessage ="El campo {0} es obligatorio.")]
-        [MaxLength(100, ErrorMessage ="El campo {0} no puede tener mas de {1} caracteres.")]
-        [Display(Name ="Pais")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres.")]
+        [Display(Name = "Pais")]
         public string Name { get; set; } = null!;
     }
 }
