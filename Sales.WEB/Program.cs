78775732c8df7f6a3efa1c5125d48f51548a5be7
Servando Hernandez Hernandez <s_musicpia@yahoo.com.mx>
2023-09-12 16:43:13 -0600

    Tutorial 69 - Parte 03 - Creando el primer componente en Blazor

--- Sales.WEB/Program.cs
+++ Sales.WEB/Program.cs
@@ -10,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7048/") });
+builder.Services.AddScoped<IRepository, Repository>();
 
 await builder.Build().RunAsync();
